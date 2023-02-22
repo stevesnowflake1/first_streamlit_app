689e2905ea4d6815a002483058fcb5422e26fd3a
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-22 11:57:29 -0600

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
-streamlit.header('🥣Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled & Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smothie')
+streamlit.text('🐔Hard-Boiled & Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
 
