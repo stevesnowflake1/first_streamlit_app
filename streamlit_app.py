59bda404381302cb41cd9f83f03170c4b71045d0
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-22 11:42:40 -0600

    Update streamlit_app.py
    Corrected mispelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omege 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smothie')
 streamlit.text('Hard-Boiled & Free-Range Egg')
 
