1cdba1fe4b4ffbebfc731f46ec4eeb66e32523a7
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-22 11:38:44 -0600

    Update streamlit_app.py
    Changed to Single qoute

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 
-streamlit.title("My parents New Healthy Dinner")
-streamlit.header("Breakfast Menu")
+streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+
