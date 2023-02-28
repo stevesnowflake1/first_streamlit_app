75d4fd0b2621513d993a08f2a9bfefea12ef1d5f
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-28 13:42:02 -0600

    added to show fruit typed

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+fruits_to_show = my_fruit_list.loc[fruit_choice]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
