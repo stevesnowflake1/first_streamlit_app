dc928d8988ce5c8341949b259b4551e70294e381
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-28 13:11:18 -0600

    Update streamlit_app.py
    Added json to response

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,10 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+#New Section to display fruitivice response
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
