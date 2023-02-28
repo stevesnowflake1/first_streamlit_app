fc297289b9448388c3f918450f8f1cf25013c14e
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-02-28 13:34:41 -0600

    Added user prompt to ask for the fruit to display

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display nornalized result as a table
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
