fe0a7602a381ce77436bd0162f654153a44c2b21
stevesnowflake1 <126098169+stevesnowflake1@users.noreply.github.com>
2023-03-01 13:49:55 -0600

    Added function for snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,21 +57,30 @@ try:
 except URLError as e:
   streamlit.error()
 
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_data_row = my_cur.fetchone()
+# my_data_rows = my_cur.fetchall() # get all data rows
+# streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
+# Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load First'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list() # get all data rows
+    streamlit.dataframe(my_data_rows) 
+#streamlit.text(my_data_row)
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 streamlit.write('This should not run')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall() # get all data rows
-# streamlit.text("Hello from Snowflake:")
-#streamlit.text("The fruit load list contains:")
-#streamlit.text(my_data_row)
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 # Allow end user to add fruit to the list
 add_my_fuit = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('Thank you for adding ', add_my_fuit)
